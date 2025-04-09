7e1ed86fbcdc93f0a3468f62b6057df79552a520
Isaya Otieno <isayaotieno@Isayas-Air.attlocal.net>
2025-04-08 23:31:40 -0400

    second commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js", // the main entry file for your application
   output: {
     filename: "bundle.js", // the output bundle file name
     path: path.resolve(__dirname, "dist"), // use "dist" or desired build directory
+    clean: true,
   },
   module: {
     rules: [
@@ -22,6 +24,10 @@ module.exports = {
     ],
   },
   plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html", // take the HTML file from public folder
+      filename: "index.html", // output name in the dist folder
+    }),
     new Dotenv() // 👈 this injects environment variables from .env
   ],
 };
